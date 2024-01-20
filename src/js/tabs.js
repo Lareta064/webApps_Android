459486efc8bf90a7449445466d0e5892da90dd1d
Lareta064 <lareta064@gmail.com>
2024-01-20 15:40:53 +0300

    fix-ui

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -2,23 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
 	//====== each group and toggle active class ========
 	const tabs = document.querySelectorAll('.tabs');
 	// На нет и скрипта нет
-	if(tabs.length < 1) return;
+	if (tabs.length < 1) return;
 	// Раз дошёл сюда, значит табс не пустой
-	for (let tab of tabs)
-	{
+	for (let tab of tabs) {
 		// Ищем кнопки и контейнеры
 		const tabBtns = tab.querySelectorAll('[tab-btn]');
 		const tabContainers = tab.querySelectorAll('[tab-body]');
 
 		// Если количество кнопок и контейнеров разное или если их нет
-		if(
-			tabBtns.length !== tabContainers.length 
+		if (
+			tabBtns.length !== tabContainers.length
 			|| tabBtns.length === 0
 		) return;
 
 		// Добавляем листенеры
-		for(btn of tabBtns)
-		{
+		for (btn of tabBtns) {
 			const clickedBtn = btn;
 			btn.addEventListener('click', (e) => {
 				e.stopPropagation();
@@ -28,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 				const activeCont = Array.from(tabContainers).find(cont => cont.classList.contains('active'));
 				activeBtn.classList.remove('active');
 				activeCont.classList.remove('active');
-				
+
 				const relatedCont = Array.from(tabContainers).find(cont => cont.getAttribute('tab-body') === btnData);
 				clickedBtn.classList.add('active');
 				relatedCont.classList.add('active');
