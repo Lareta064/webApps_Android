b395651025d03ebed4b65817eb9bddd153940fd9
Lareta064 <lareta064@gmail.com>
2024-01-22 21:39:20 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -144,4 +144,33 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		}
 	}
+
+	/*===================CUSTOM DROP FILTER====================*/
+	const dropFilters = document.querySelectorAll('.drop-filter');
+	if (dropFilters.length > 0) {
+		for (let item of dropFilters) {
+			const itemSelect = item.querySelector('.drop-filter__input');
+			const itemDrop = item.querySelector('.drop-filter__list');
+			// const itemDropItems = item.querySelectorAll('.drop-filter__item');
+
+			item.addEventListener('click', function (e) {
+
+				if (this.classList.contains('active')) {
+					this.classList.remove('active');
+				} else {
+					this.classList.add('active');
+				}
+			});
+			/********click Drop item******* */
+			if (itemDrop) {
+				itemDrop.addEventListener('click', function (e) {
+					e.stopPropagation();
+					const selectItemText = e.target.textContent;
+					itemSelect.querySelector('input').setAttribute('value', selectItemText);
+					item.classList.remove('active');
+				});
+			}
+		}
+	}
+
 });
